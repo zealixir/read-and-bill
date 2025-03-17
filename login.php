10d1a1830de0fc508ce0f91b6fb885ab8aaaed2c
Edzel Legaspi <edzel.legaspi@evsu.edu.ph>
2025-03-17 11:53:16 +0800

    created login form

---
+++ login.php
@@ -0,0 +1,13 @@
+<?php
+    include "upper.php";
+    upperHTML("login", "pin-login");
+?>
+<body>
+    <form class="login">
+        <h1>LOGIN</h1>
+        <input type="text" placeholder="Username">
+        <input type="password" placeholder="Password">
+        <button>Login</button>
+    </form>  
+</body>
+</html>
